9e30d3c9b8f155e45e4a1f21032fe343dbc7cec6
isaacparsons <isaac.2962@gmail.com>
2024-08-25 18:30:27 -0600

    stuff

---
+++ brew/install.sh
@@ -0,0 +1,10 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install awscli
+brew install redis
+brew install node
+brew install nmap
+brew install pyenv
+brew install postgresql
+brew install terraform
+brew install youtube-dl
---
+++ zsh/.zshrc
@@ -0,0 +1,2 @@
+export ZSH="/Users/isaacparsons/.oh-my-zsh"
+# source ~/.bash_profile
