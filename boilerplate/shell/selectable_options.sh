26523036c204af7febddefd8a6354d32b35b3289
Isaac Parsons <isaac.parsons@tokusystems.com>
2024-09-16 11:46:20 -0600

    shell

--- boilerplate/shell/selectable_options.sh
+++ boilerplate/shell/selectable_options.sh
@@ -1,50 +1,35 @@
-#!/bin/bash
+#!/bin/sh
 
-# Array of menu items
-menu_items=("Option 1" "Option 2" "Option 3" "Exit")
-
-# Function to draw menu
 draw_menu() {
     local current_row=$1
+    local i=("$@")
+    local items=(${i[@]:1})
 
     clear
-    echo "Use up/down arrow keys to move and Enter to select:"
+    echo "Use up/down arrow keys to move and Enter to select a file:"
     echo
 
-    for i in "${!menu_items[@]}"; do
+    for i in "${!items[@]}"; do
         if [[ $i -eq $current_row ]]; then
-	    tput setaf 0
-	    tput setab 7
-            echo -e "> ${menu_items[$i]}"
-	    tput sgr0
+            tput setaf 0  # Set text color to black
+            tput setab 7  # Set background color to white
+            echo "> ${items[$i]}"
+            tput sgr0     # Reset colors
         else
-            echo "  ${menu_items[$i]}"
+            echo "  ${items[$i]}"
         fi
     done
 }
 
-# Function to handle the selected option
-handle_option() {
-    case $1 in
-        0) echo "You selected Option 1" ;;
-        1) echo "You selected Option 2" ;;
-        2) echo "You selected Option 3" ;;
-        3) echo "Exiting..."; exit 0 ;;
-    esac
-    read -n 1 -s -r -p "Press any key to continue"
-}
-
-# Main function
-main() {
+interactive_menu() {
+    local items=("$@")
     local current_row=0
     local key=""
-
-    # Hide cursor
-    tput civis
     
-    # Capture key presses
+    tput civis # Hide cursor
+    
     while true; do
-        draw_menu $current_row
+        draw_menu "$current_row" "${items[@]}"
 
         # Read a single character
         read -s -n1 key
@@ -54,7 +39,12 @@ main() {
             A) ((current_row--)) ;; # Up arrow
             B) ((current_row++)) ;; # Down arrow
             '') # Enter key
-                handle_option $current_row
+                tput cnorm  # Show cursor
+                return "$current_row"
+                ;;
+            q|Q) # Quit option
+                tput cnorm  # Show cursor
+                return 255
                 ;;
             $'\e') # Escape sequence
                 read -s -n2 key
@@ -65,17 +55,49 @@ main() {
                 ;;
         esac
 
-        # Ensure current_row stays within bounds
         if (( current_row < 0 )); then
-            current_row=$((${#menu_items[@]}-1))
-        elif (( current_row >= ${#menu_items[@]} )); then
+            current_row=$((${#items[@]}-1))
+        elif (( current_row >= ${#items[@]} )); then
             current_row=0
         fi
     done
-
-    # Show cursor
-    tput cnorm
 }
 
-# Run the main function
-main
+
+
+
+
+db_name=e2e-kit-sqldb-1
+directory_path="/Users/isaacparsons/Desktop/db_stuff/db_dumps"
+tables=("assets" "asset_types")
+
+if [ ! -d "$directory_path" ]; then
+    echo "Error: Directory $directory_path does not exist."
+    exit 1
+fi
+
+cd "$directory_path" || exit
+
+# Read files into an array
+files=()
+for file in *; do
+    if [ -f "$file" ]; then
+        files+=("$file")
+    fi
+done
+files+=("Quit")
+
+interactive_menu "${files[@]}"
+
+selected=$?
+
+if [ $selected -eq 255 ] || [ "${files[selected]}" = "Quit" ]; then
+    echo "You chose to quit."
+    exit 0
+else
+   # do stuff
+fi
+
+
+
+
